7ee3c2071908164852bd3121136a8a7d454a2326
Niels van Adrichem <niels.vanadrichem@tno.nl>
2017-01-02 11:52:13 +0100

    First commit

---
+++ start_Multiswitch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#PYTHONPATH=. ./bin/ryu-manager --verbose --observe-links ryu/app/ForwardingMultiSwitch.py
+PYTHONPATH=. ./bin/ryu-manager --observe-links ryu/app/ForwardingMultiSwitch.py
+
